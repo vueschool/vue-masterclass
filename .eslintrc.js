6371972dc52145aff4f3921071112964a6500105
Alex Kyriakidis <hootlex@icloud.com>
2020-12-30 14:50:17 +0100

    update eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "root": true,
+    "env": {
+    "node": true
+  },
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended"
+  ],
+    "parserOptions": {
+    "parser": "babel-eslint"
+  },
+  "rules": {
+    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn"
+  }
+}
