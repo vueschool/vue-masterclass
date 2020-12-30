2e1568f786049c7bea7fe489872f5e9d6420e20b
Alex Kyriakidis <hootlex@icloud.com>
2020-12-30 15:18:10 +0100

    install eslint standard using vue cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
-  "root": true,
-    "env": {
-    "node": true
+  root: true,
+
+  env: {
+    node: true
   },
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended"
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
   ],
-    "parserOptions": {
-    "parser": "babel-eslint"
+
+  parserOptions: {
+    parser: 'babel-eslint'
   },
-  "rules": {
-    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "warn"
+
+  rules: {
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
