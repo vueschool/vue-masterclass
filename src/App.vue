7d7b141e69616467eb93852e20b48102a9d42055
Alex Kyriakidis <hootlex@icloud.com>
2021-01-08 16:38:31 +0100

    render threads and posts

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <page-home/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PageHome from '@/components/PageHome'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PageHome
   }
 }
 </script>
---
+++ src/components/PageHome.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>Hello from page home</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{thread.title}}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{userById(postById(postId).userId).name}}</p>
+      <p>{{postById(postId).text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find(p => p.id === postId)
+    },
+    userById (userId) {
+      return this.users.find(p => p.id === userId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
