f90dc01db399f51731553339141d99505cc3f802
Alex Kyriakidis <hootlex@icloud.com>
2021-01-31 14:45:19 +0100

    global styles and stylesheet processing

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/style.css";
 </style>
