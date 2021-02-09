4ffd4abeef5d43337fd0f1758980070703c8f6a6
Alex Kyriakidis <hootlex@icloud.com>
2021-02-09 16:36:20 +0100

    Router Links

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -8,7 +8,7 @@
       <div v-for="thread in threads" :key="thread.id" class="thread">
         <div>
           <p>
-            <a href="#">{{ thread.title }}</a>
+            <router-link :to="{name: 'ThreadShow', params: {id: thread.id}}">{{ thread.title }}</router-link>
           </p>
           <p class="text-faded text-xsmall">
             By <a href="#">{{ userById(thread.userId).name }}</a>, {{ thread.publishedAt }}.
