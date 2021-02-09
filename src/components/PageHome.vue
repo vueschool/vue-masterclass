9658c711dde923c9d517d72027fac5a60354fad0
Alex Kyriakidis <hootlex@icloud.com>
2021-02-09 16:24:17 +0100

    create ThreadList.vue and clean up homepage

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,61 +1,18 @@
 <template>
-  <div v-for="thread in threads"
-       :key="thread.id"
-       class="col-large push-top"
-  >
-    <h1>{{ thread.title }}</h1>
-
-    <div class="post-list">
-      <div class="post"
-           v-for="postId in thread.posts"
-           :key="postId"
-      >
-
-        <div class="user-info">
-          <a href="#" class="user-name">{{userById(postById(postId).userId).name}}</a>
-
-          <a href="#">
-            <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="">
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>
-              {{postById(postId).text}}
-            </p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{postById(postId).publishedAt}}
-        </div>
-
-      </div>
-
-    </div>
-  </div>
+  <h1>Welcome to the Forum</h1>
+  <ThreadList :threads="threads"/>
 </template>
 
 <script>
+import ThreadList from '@/components/ThreadList'
 import sourceData from '@/data.json'
 export default {
+  components: {
+    ThreadList
+  },
   data () {
     return {
-      threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users
-    }
-  },
-  methods: {
-    postById (postId) {
-      return this.posts.find(p => p.id === postId)
-    },
-    userById (userId) {
-      return this.users.find(p => p.id === userId)
+      threads: sourceData.threads
     }
   }
 }
---
+++ src/components/ThreadList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="col-full">
+
+    <div class="thread-list">
+
+      <h2 class="list-title">Threads</h2>
+
+      <div v-for="thread in threads" :key="thread.id" class="thread">
+        <div>
+          <p>
+            <a href="#">{{ thread.title }}</a>
+          </p>
+          <p class="text-faded text-xsmall">
+            By <a href="#">{{ userById(thread.userId).name }}</a>, {{ thread.publishedAt }}.
+          </p>
+        </div>
+
+        <div class="activity">
+          <p class="replies-count">
+           {{ thread.posts.length }} replies
+          </p>
+
+          <img class="avatar-medium" :src="userById(thread.userId).avatar" alt="">
+
+          <div>
+            <p class="text-xsmall">
+              <a href="#">{{ userById(thread.userId).name }}</a>
+            </p>
+            <p class="text-xsmall text-faded">{{ thread.publishedAt }}</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+  props: {
+    threads: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find(p => p.id === postId)
+    },
+    userById (userId) {
+      return this.users.find(p => p.id === userId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
