bee458f063cec21cc173c0bcf6cf3bc258d83c08
Daniel Kelly <me@danielkelly.io>
2021-04-20 13:29:19 -0500

    50. firebase setup and installation

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: 'AIzaSyBEOEoSj1vPmcq_qSPdhQjlTdXYuz6afkM',
+  authDomain: 'vue-school-forum-19467.firebaseapp.com',
+  projectId: 'vue-school-forum-19467',
+  storageBucket: 'vue-school-forum-19467.appspot.com',
+  messagingSenderId: '49977500509',
+  appId: '1:49977500509:web:7ce08908132b743fb0beb4'
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
+import firebase from 'firebase'
+import firebaseConfig from '@/config/firebase'
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 const forumApp = createApp(App)
 forumApp.use(router)
