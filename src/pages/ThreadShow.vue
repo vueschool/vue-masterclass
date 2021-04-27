39ba788c56c3fe73f238098262e5145d1b52ee72
Daniel Kelly <me@danielkelly.io>
2021-04-27 15:36:31 -0500

    56. creating vuex actions that fetch multiple items from the cloud firestore

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -71,10 +71,10 @@ export default {
     this.$store.dispatch('fetchUser', { id: thread.userId })
 
     // fetch the posts
-    thread.posts.forEach(async (postId) => {
-      const post = await this.$store.dispatch('fetchPost', { id: postId })
-      this.$store.dispatch('fetchUser', { id: post.userId })
-    })
+    const posts = await this.$store.dispatch('fetchPosts', { ids: thread.posts })
+    // fetch the users associated with the posts
+    const users = posts.map(post => post.userId)
+    this.$store.dispatch('fetchUsers', { ids: users })
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -94,6 +94,15 @@ export default createStore({
     fetchPost ({ dispatch }, { id }) {
       return dispatch('fetchItem', { resource: 'posts', id, emoji: '💬' })
     },
+    fetchThreads ({ dispatch }, { ids }) {
+      return dispatch('fetchItems', { resource: 'threads', ids, emoji: '📄' })
+    },
+    fetchUsers ({ dispatch }, { ids }) {
+      return dispatch('fetchItems', { resource: 'users', ids, emoji: '🙋' })
+    },
+    fetchPosts ({ dispatch }, { ids }) {
+      return dispatch('fetchItems', { resource: 'posts', ids, emoji: '💬' })
+    },
     fetchItem ({ state, commit }, { id, emoji, resource }) {
       console.log('🔥', emoji, id)
       return new Promise((resolve) => {
@@ -103,6 +112,9 @@ export default createStore({
           resolve(item)
         })
       })
+    },
+    fetchItems ({ dispatch }, { ids, resource, emoji }) {
+      return Promise.all(ids.map(id => dispatch('fetchItem', { id, resource, emoji })))
     }
   },
   mutations: {
