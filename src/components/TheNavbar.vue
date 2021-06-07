8e7c5c2b55aac3cac771e0d6c0704bdbe292f589
Daniel Kelly <me@danielkelly.io>
2021-06-07 18:29:56 -0500

    81. using class bindings for user data drop down menu

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -16,25 +16,24 @@
     <nav class="navbar">
       <ul>
         <li v-if="authUser" class="navbar-user">
-          <router-link :to="{name:'Profile'}">
+          <a @click.prevent="userDropdownOpen = !userDropdownOpen">
             <img class="avatar-small" :src="authUser.avatar" :alt="`${authUser.name} profile picture`"/>
             <span>
                 {{authUser.name}}
                 <img class="icon-profile" src="../assets/svg/arrow-profile.svg" alt=""/>
             </span>
-          </router-link>
+          </a>
 
           <!-- dropdown menu -->
           <!-- add class "active-drop" to show the dropdown -->
-          <div id="user-dropdown">
+          <div id="user-dropdown" :class="{'active-drop': userDropdownOpen}">
             <div class="triangle-drop"></div>
             <ul class="dropdown-menu">
-              <li class="dropdown-menu-item"><a href="profile.html">View profile</a></li>
-              <li class="dropdown-menu-item"><a href="#">Log out</a></li>
+              <li class="dropdown-menu-item"><router-link :to="{name: 'Profile'}">View profile</router-link></li>
+              <li class="dropdown-menu-item"><a @click.prevent="$store.dispatch('signOut')">Sign Out</a></li>
             </ul>
           </div>
         </li>
-        <li v-if="authUser" class="navbar-item"><a @click.prevent="$store.dispatch('signOut')">Sign Out</a></li>
         <li v-if="!authUser" class="navbar-item"><router-link :to="{name: 'SignIn'}">Sign In</router-link></li>
         <li v-if="!authUser" class="navbar-item"><router-link :to="{name: 'Register'}">Register</router-link></li>
       </ul>
@@ -67,6 +66,11 @@
 <script>
 import { mapGetters } from 'vuex'
 export default {
+  data () {
+    return {
+      userDropdownOpen: false
+    }
+  },
   computed: {
     ...mapGetters(['authUser'])
   }
