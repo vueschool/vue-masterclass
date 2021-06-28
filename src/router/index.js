f3251748f695a48a0f0aa8e7e1f0f37d589f6aa0
Daniel Kelly <me@danielkelly.io>
2021-06-28 10:41:56 -0500

    88b. hiding guest pages from logged in users

--- src/router/index.js
+++ src/router/index.js
@@ -83,12 +83,14 @@ const routes = [
   {
     path: '/register',
     name: 'Register',
-    component: Register
+    component: Register,
+    meta: { requiresGuest: true }
   },
   {
     path: '/signin',
     name: 'SignIn',
-    component: SignIn
+    component: SignIn,
+    meta: { requiresGuest: true }
   },
   {
     path: '/logout',
@@ -119,6 +121,9 @@ router.beforeEach(async (to, from) => {
   console.log(`🚦 navigating to ${to.name} from ${from.name}`)
   store.dispatch('unsubscribeAllSnapshots')
   if (to.meta.requiresAuth && !store.state.authId) {
+    return { name: 'SignIn' }
+  }
+  if (to.meta.requiresGuest && store.state.authId) {
     return { name: 'Home' }
   }
 })
