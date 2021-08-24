7ead9f55fdba8b607c3152b8890352d57e57f47f
Daniel Kelly <me@danielkelly.io>
2021-08-24 14:37:48 -0500

    104b using the transition group component

--- src/components/AppNotifications.vue
+++ src/components/AppNotifications.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="notifications">
-    <div
-      class="notification"
-      v-for="notification in notifications"
-      :key="notification.id"
-    >
-      <span>{{ notification.message }}</span>
-      <button @click="removeNotification(notification.id)">x</button>
-    </div>
+    <transition-group name="notification">
+      <div
+        class="notification"
+        v-for="notification in notifications"
+        :key="notification.id"
+      >
+        <span>{{ notification.message }}</span>
+        <button @click="removeNotification(notification.id)">x</button>
+      </div>
+    </transition-group>
   </div>
 </template>
 <script>
@@ -20,7 +22,7 @@ export default {
 }
 </script>
 <style scoped>
-  .notifications {
+.notifications {
   position: fixed;
   bottom: 20px;
   right: 0;
@@ -34,5 +36,17 @@ export default {
   padding: 10px 20px;
   margin-bottom: 5px;
   border-left: 5px solid #263959;
+}
+.notification-enter-active,
+.notification-leave-active {
+  transition: all .5s ease;
+}
+.notification-enter-from,
+.notification-leave-to {
+  opacity: 0;
+  transform: translateX(100%);
+}
+.notification-move {
+  transition: transform 0.8s ease;
 }
 </style>
