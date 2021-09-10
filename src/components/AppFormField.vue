35d39b0486d59bfbd47121e913edaee4e0982c06
Daniel Kelly <me@danielkelly.io>
2021-09-10 15:17:54 -0500

    wip

---
+++ src/components/AppFormField.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="form-group">
+    <label :for="name">{{ label }}</label>
+    <VeeField
+      :modelValue="modelValue"
+      @input="$emit('modelUpdate', $event)"
+      :label="label"
+      :name="name"
+      :id="name"
+      v-bind="$attrs"
+      v-slot="{ field, errorMessage }"
+    >
+      <input v-bind="{...field, ...$attrs}" :class="{ 'input-error': errorMessage }" class="form-input"/>
+      <VeeErrorMessage :name="name" class="form-error" />
+    </VeeField>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    modelValue: { type: String, default: '' },
+    name: { type: String, required: true },
+    label: { type: String, required: true }
+  }
+}
+</script>
---
+++ src/plugins/VeeValidatePlugin.js
@@ -0,0 +1,41 @@
+import { Form, Field, ErrorMessage, defineRule, configure } from 'vee-validate'
+import { required, email, min } from '@vee-validate/rules'
+import { localize } from '@vee-validate/i18n'
+import { mapValues } from 'lodash'
+
+const rules = {
+  required: {
+    test: required,
+    message: '{field} is required'
+  },
+  email: {
+    test: email,
+    message: '{field} must be a valid email address'
+  },
+  min: {
+    test: min,
+    message: '{field} must be a minimum of 0:{min} characters'
+  }
+}
+
+export default (app) => {
+  generateMessages()
+  defineRules()
+  app.component('VeeForm', Form)
+  app.component('VeeField', Field)
+  app.component('VeeErrorMessage', ErrorMessage)
+}
+
+function generateMessages () {
+  configure({
+    generateMessage: localize('en', {
+      messages: mapValues(rules, rule => rule.message)
+    })
+  })
+}
+
+function defineRules () {
+  Object.keys(rules).forEach(
+    rule => defineRule(rule, rules[rule].test)
+  )
+}
