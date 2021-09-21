d3b0e3962af30818a1b5727413e1f2220594bc28
Daniel Kelly <me@danielkelly.io>
2021-09-21 13:40:45 -0500

    113b. using multiple rules on a single field

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -18,7 +18,10 @@
 
         <div class="form-group">
           <label for="email">Email</label>
-          <VeeField name="email" v-model="form.email" id="email" type="email" class="form-input" />
+          <VeeField name="email" v-model="form.email" id="email" type="email" class="form-input"
+            rules="required|email"
+            />
+            <VeeErrorMessage name="email" class="form-error" />
         </div>
 
         <div class="form-group">
--- src/plugins/VeeValidatePlugin.js
+++ src/plugins/VeeValidatePlugin.js
@@ -1,7 +1,8 @@
 import { Form, Field, ErrorMessage, defineRule } from 'vee-validate'
-import { required } from '@vee-validate/rules'
+import { required, email } from '@vee-validate/rules'
 export default (app) => {
   defineRule('required', required)
+  defineRule('email', email)
 
   app.component('VeeForm', Form)
   app.component('VeeField', Field)
