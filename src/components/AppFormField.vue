b41aacbe469a188e174e6654cc23d24688471538
Daniel Kelly <me@danielkelly.io>
2021-09-24 16:20:37 -0500

    116 wrapping VeeValidate field with custom AppFormField

---
+++ src/components/AppFormField.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="form-group">
+    <label :for="name">{{label}}</label>
+    <VeeField
+      :name="name"
+      :label="label"
+      :modelValue="modelValue"
+      @input="$emit('modelUpdate', $event)"
+      :id="name"
+      class="form-input"
+      v-bind="$attrs"
+    />
+    <VeeErrorMessage :name="name" class="form-error" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: { type: String, required: true },
+    label: { type: String, required: true },
+    modelValue: { type: String, default: '' }
+  }
+}
+</script>
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -4,33 +4,10 @@
       <VeeForm @submit="register" class="card card-form">
         <h1 class="text-center">Register</h1>
 
-        <div class="form-group">
-          <label for="name">Full Name</label>
-          <VeeField name="name" v-model="form.name" id="name" type="text" class="form-input" rules="required" />
-          <VeeErrorMessage name="name" class="form-error" />
-        </div>
-
-        <div class="form-group">
-          <label for="username">Username</label>
-          <VeeField name="username" v-model="form.username" id="username" type="text" class="form-input" rules="required" />
-          <VeeErrorMessage name="username" class="form-error" />
-        </div>
-
-        <div class="form-group">
-          <label for="email">Email</label>
-          <VeeField name="email" v-model="form.email" id="email" type="email" class="form-input"
-            rules="required|email"
-            />
-            <VeeErrorMessage name="email" class="form-error" />
-        </div>
-
-        <div class="form-group">
-          <label for="password">Password</label>
-          <VeeField name="password" label="Password" v-model="form.password" id="password" type="password" class="form-input"
-            rules="required|min:8"
-            />
-          <VeeErrorMessage name="password" class="form-error" />
-        </div>
+        <AppFormField v-model="form.name" name="name" label="Name" rules="required" />
+        <AppFormField v-model="form.username" name="username" label="Username" rules="required" />
+        <AppFormField v-model="form.email" name="email" label="Email" rules="required|email" type="email" />
+        <AppFormField v-model="form.password" name="password" label="Password" rules="required|min:8" type="password" />
 
         <div class="form-group">
           <label for="avatar">
